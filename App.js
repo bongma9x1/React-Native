dcbf04ace63642e6e24e81565337b1d03c258b3f
bongma9x1 <doannv19@gmail.com>
2019-01-21 14:24:12 +0700

    state trong React native
    thay doi 1 so sau khi an

--- App.js
+++ App.js
@@ -12,12 +12,22 @@ import {StatusBar, StyleSheet, Text, View, AppRegistry} from 'react-native';
 class DoanReact extends Component{
   constructor(props){
     super(props);
+    this.state = {somaynam:999}
+  }
+  clickMe(){
+    console.log('CLICK ME')
+    this.setState({
+      somaynam:this.state.somaynam + 1
+      });
   }
   render(){
     return(
       <View>
-        <StatusBar hindden = "true"/>
-        <Text style = {{Backgr}}>Doan aaaaaaafasfasaaa </Text>
+        <Text style = {{fontSize:100}}>{this.state.somaynam} </Text>
+          <TouchableOpacity onPress = {()=>{this.clickMe()}}>
+            <View stye = {{with:100, height:200, backgroundColor: 'red'}}>
+            </View>
+          </TouchableOpacity>
       </View>
       );
   }
